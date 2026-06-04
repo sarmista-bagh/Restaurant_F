6b554c727c45c22c6f4898a967f6eaf31ae2ddac
sarmista-bagh <sarmistabagh88@gmail.com>
2026-06-04 17:39:15 +0530

    Update API endpoint for fetching restaurant data

--- Frontend/client/src/pages/Home.jsx
+++ Frontend/client/src/pages/Home.jsx
@@ -27,8 +27,8 @@ const Home = () => {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get(
-        "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING",
+       const response = await axios.get(
+        "https://menu-gdbm.onrender.com/api/restaurants",
       );
 
       const json = response.data;
